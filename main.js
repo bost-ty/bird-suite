dff726da6b1a835433ecb05e6e4b70c38e6c24da
bost-ty <61333914+bost-ty@users.noreply.github.com>
2024-10-06 16:29:25 -0700

    Auth works

--- main.js
+++ main.js
@@ -1,75 +1,107 @@
 "use strict";
 
-/* --------- Constants -------- */
-
 const CLIENT_ID = "p1qg8d7u9wf5580xqwuu3ot0yk69nx";
 const REDIRECT_URI = "http://localhost:5500";
 const STATE_STORE = `oauth_state_${CLIENT_ID}`;
-const SCOPES = ["moderator:read:chatters"];
-
-/* --------- HTML Elements -------- */
-
+const SCOPES = ["moderator:read:chatters", "channel:read:polls"];
 const authorizeButton = document.getElementById("authorizeButton");
 const authorizeSection = document.getElementById("authorizeSection");
-
 const callbackSection = document.getElementById("callbackSection");
+const controlsSection = document.getElementById("controlsSection");
 
-/* --------- Good ol' imperative code -------- */
-
-const sp = new URLSearchParams(document.location.hash);
-const sessionState = sessionStorage.getItem(STATE_STORE);
-const uriState = sp.get("state");
-
-if (hasValidStates(sessionState, uriState)) {
-	callbackSection.innerHTML = "It works, make something good happen in here.";
-} else {
-	callbackSection.innerHTML =
-		"There's a problem with your authorization. Try again, or bail.";
-}
-
-/* --------- Event Listeners -------- */
+const hideButton = document.getElementById("ctrl-hide");
+hideButton.onclick = () => toggleVisibility(authorizeSection, callbackSection);
 
 authorizeButton?.addEventListener("click", () => {
-	const [parameters, state] = generateAuthParameters(
-		"token",
-		CLIENT_ID,
-		REDIRECT_URI,
-		SCOPES,
-		window.btoa(new Date().toISOString())
-	);
+	const responseType = "token";
+	const forceVerify = true;
+	const scopes = SCOPES.map((scope) => encodeURIComponent(scope)).join("+");
+	const state = window.btoa(new Date().toISOString());
+	const parameters = `response_type=${responseType}
+		&client_id=${CLIENT_ID}
+		&force_verify=${forceVerify}
+		&redirect_uri=${REDIRECT_URI}
+		&scope=${scopes}
+		&state=${state}
+		`;
 	sessionStorage.setItem(STATE_STORE, state);
 	const url = `https://id.twitch.tv/oauth2/authorize?${parameters}`;
 	location.assign(url);
 });
 
-/* --------- Functions -------- */
+if (!document.location.hash) throw new Error("Halting: no document.location.hash");
 
-/**
- * Generate usable `parameters` and `state` for making Twitch auth requests.
- * @param {string} responseType 'token'
- * @param {string} clientId CLIENT_ID
- * @param {string} redirectUri REDIRECT_URI
- * @param {string[]} scopes array of strings for requested scopes
- * @param {string} state `Window.btoa(new Date().toISOString())`
- * @returns {string[]}
- */
-function generateAuthParameters(responseType, clientId, redirectUri, scopes, state) {
-	return [
-		`response_type=${responseType}
-		&client_id=${clientId}
-		&redirect_uri=${redirectUri}
-		&scope=${encodeURIComponent(scopes.join(" "))}
-		&state=${state}
-		`,
-		state,
-	];
+/* Authorized! */
+
+const sp = new URLSearchParams(document.location.hash);
+const sessionState = sessionStorage.getItem(STATE_STORE);
+const uriState = sp.get("state");
+
+if (!(uriState && sessionState && uriState === sessionState)) {
+	callbackSection.style.color = "var(--code)";
+	callbackSection.style.fontFamily = "monospace";
+	callbackSection.textContent =
+		"Client/server OAuth state mismatch. Please reauthenticate.";
+	throw new Error("Client/server OAuth state mismatch");
+} else if (sp.has("error")) {
+	throw new Error(sp.get("error"));
+} else {
+	const validAuth = fetch("https://id.twitch.tv/oauth2/validate", {
+		headers: { Authorization: `OAuth ${sp.get("#access_token")}` },
+	}).then((res) => res);
+	if (!validAuth) {
+		throw new Error("Invalid OAuth response from Twitch");
+	}
+	authorizeButton.innerText = "Reauthorize on Twitch";
+	renderURLSearchParamsAsUL(callbackSection, sp, [
+		"#access_token",
+		"scope",
+		"state",
+		"token_type",
+	]);
 }
-/**
- *
- * @param {string} sessionState
- * @param {string} uriState
- * @returns {boolean}
- */
-function hasValidStates(sessionState, uriState) {
-	return uriState && sessionState && uriState === sessionState;
+
+function renderURLSearchParamsAsUL(parentElement, urlSearchParams, keyWhitelist) {
+	const ul = document.createElement("ul");
+	urlSearchParams.forEach((value, key) => {
+		const li = document.createElement("li");
+		if (keyWhitelist.includes(key)) {
+			li.innerHTML = `<strong>${key}</strong> ${value}`;
+			ul.append(li);
+		}
+	});
+	parentElement.insertAdjacentElement("beforeend", ul);
+}
+
+function toggleVisibility(...elements) {
+	elements.forEach((element) => {
+		const dur = 250;
+		const isHidden = element.hidden;
+		if (!element.style.transitionProperty)
+			element.style.transitionProperty = "opacity, transform";
+		if (!element.style.transitionDuration)
+			element.style.transitionDuration = `${dur}ms`;
+		if (!element.style.transitionTimingFunction)
+			element.style.transitionTimingFunction = "ease";
+		let transitionTimeout = setTimeout(() => {}, 0);
+		if (isHidden) {
+			clearTimeout(transitionTimeout);
+			element.style.opacity = 0;
+			element.style.transform = "translateY(-1rem)";
+			element.hidden = !element.hidden;
+			transitionTimeout = setTimeout(() => {
+				element.style.opacity = 1;
+				element.style.transform = "translateY(0) ";
+			}, dur);
+		} else if (!isHidden) {
+			clearTimeout(transitionTimeout);
+			element.style.transform = "translateY(-1rem)";
+			element.style.opacity = 0;
+			transitionTimeout = setTimeout(() => {
+				element.hidden = !element.hidden;
+			}, dur + 100);
+		} else {
+			throw new Error(`toggleVisibility couldn't evaluate isHidden for ${element}`);
+		}
+	});
 }
