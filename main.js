3ce3e6c364a14c00dd07a69c03337971f9fd290b
bost-ty <61333914+bost-ty@users.noreply.github.com>
2024-10-07 18:54:37 -0700

    Viewers, start Chatters

--- main.js
+++ main.js
@@ -1,16 +1,80 @@
 "use strict";
 
+import { makeConfig, generateCsv, downloadCsv } from "./csv.js";
+
 const CLIENT_ID = "p1qg8d7u9wf5580xqwuu3ot0yk69nx";
 const REDIRECT_URI = "http://localhost:5500";
 const STATE_STORE = `oauth_state_${CLIENT_ID}`;
-const SCOPES = ["moderator:read:chatters", "channel:read:polls"];
+const EVENT_STORE = `events_${CLIENT_ID}`;
+const SCOPES = ["moderator:read:chatters"];
+const HELIX = "https://api.twitch.tv/helix/";
+
+let events = JSON.parse(localStorage.getItem(EVENT_STORE)) || [];
+
 const authorizeButton = document.getElementById("authorizeButton");
 const authorizeSection = document.getElementById("authorizeSection");
 const callbackSection = document.getElementById("callbackSection");
-const controlsSection = document.getElementById("controlsSection");
+const connectButton = document.getElementById("connectButton");
+const markButton = document.getElementById("markButton");
+const usernameInput = document.getElementById("usernameInput");
+const eventInput = document.getElementById("eventInput");
+const eventOutput = document.getElementById("eventOutput");
+
+/**
+ * renderEvents
+ * @param {HTMLElement} parent
+ * @param {{ time: Number , data: String}} events
+ */
+function renderEvents(parent, events) {
+	console.log("RENDER EVENTS", events.length);
+	if (events.length === 0) {
+		parent.innerHTML = "";
+		return;
+	}
+	while (parent.children.length > events.length) {
+		parent.lastChild.remove();
+	}
+	events.forEach((eventObject, index) => {
+		const { time, data } = eventObject;
+		let element;
+		if (index < parent.children.length) {
+			element = parent.children[index];
+		} else {
+			element = document.createElement("div");
+			parent.appendChild(element);
+		}
+		element.innerHTML = `${new Date(time).toLocaleTimeString()}: ${data}`;
+	});
+}
+
+const rerender = () => renderEvents(eventOutput, events);
+if (events.length > 0) rerender();
+
+// Set up top-level controls
+console.log("Setting top-level controls...");
 
 const hideButton = document.getElementById("ctrl-hide");
-hideButton.onclick = () => toggleVisibility(authorizeSection, callbackSection);
+hideButton.addEventListener("click", () => {
+	toggleVisibility(authorizeSection, callbackSection);
+});
+const clearButton = document.getElementById("ctrl-clear");
+clearButton.addEventListener("click", () => {
+	let p = prompt("Are you sure?\nYES to delete ALL events.\nThis cannot be reversed!");
+	if (p === "YES") {
+		alert("You said YES; ALL events will be deleted!");
+		localStorage.removeItem(EVENT_STORE);
+		events = [];
+		rerender();
+	} else {
+		alert("You did NOT say 'YES', so did NOT delete all events.");
+	}
+});
+const clearChannelButton = document.getElementById("ctrl-clear-channel");
+clearChannelButton.addEventListener("click", () => {
+	usernameInput.disabled = false;
+});
+
+console.log("Controls set, setting authorizeButton...");
 
 authorizeButton?.addEventListener("click", () => {
 	const responseType = "token";
@@ -18,48 +82,159 @@ authorizeButton?.addEventListener("click", () => {
 	const scopes = SCOPES.map((scope) => encodeURIComponent(scope)).join("+");
 	const state = window.btoa(new Date().toISOString());
 	const parameters = `response_type=${responseType}
-		&client_id=${CLIENT_ID}
-		&force_verify=${forceVerify}
-		&redirect_uri=${REDIRECT_URI}
-		&scope=${scopes}
-		&state=${state}
-		`;
-	sessionStorage.setItem(STATE_STORE, state);
+	&client_id=${CLIENT_ID}
+	&force_verify=${forceVerify}
+	&redirect_uri=${REDIRECT_URI}
+	&scope=${scopes}
+	&state=${state}`;
+
+	localStorage.setItem(STATE_STORE, state);
 	const url = `https://id.twitch.tv/oauth2/authorize?${parameters}`;
 	location.assign(url);
 });
 
 if (!document.location.hash) throw new Error("Halting: no document.location.hash");
 
-/* Authorized! */
+/* We have /something/ in document.location.hash, let's look at it: */
 
 const sp = new URLSearchParams(document.location.hash);
-const sessionState = sessionStorage.getItem(STATE_STORE);
+const sessionState = localStorage.getItem(STATE_STORE);
 const uriState = sp.get("state");
 
 if (!(uriState && sessionState && uriState === sessionState)) {
-	callbackSection.style.color = "var(--code)";
-	callbackSection.style.fontFamily = "monospace";
-	callbackSection.textContent =
-		"Client/server OAuth state mismatch. Please reauthenticate.";
+	callbackSection.textContent = "OAuth state mismatch. Please reauthenticate!";
 	throw new Error("Client/server OAuth state mismatch");
 } else if (sp.has("error")) {
+	callbackSection.textContent = "Authentication error. Please reauthenticate!";
 	throw new Error(sp.get("error"));
-} else {
-	const validAuth = fetch("https://id.twitch.tv/oauth2/validate", {
-		headers: { Authorization: `OAuth ${sp.get("#access_token")}` },
-	}).then((res) => res);
-	if (!validAuth) {
-		throw new Error("Invalid OAuth response from Twitch");
-	}
-	authorizeButton.innerText = "Reauthorize on Twitch";
-	renderURLSearchParamsAsUL(callbackSection, sp, [
-		"#access_token",
-		"scope",
-		"state",
-		"token_type",
-	]);
 }
+
+console.log("Token response valid, starting validation & setting further controls...");
+// State is OK,
+// No authentication errors,
+// So let's maintain our OAuth session
+// and check our status w/ Twitch every hour,
+// as required.
+
+const bearer = `Bearer ${sp.get("#access_token")}`;
+const headers = { Authorization: bearer, "Client-Id": CLIENT_ID };
+
+let revalidationTimeout = revalidateAuth();
+
+async function revalidateAuth() {
+	console.log("Validating authorization...");
+	const validAuth = await fetch("https://id.twitch.tv/oauth2/validate", {
+		headers: { Authorization: bearer },
+	});
+	if (validAuth["ok"]) {
+		console.log("Validation OK!");
+		authorizeButton.innerText = "Reauthorize on Twitch";
+		renderURLSearchParamsAsUL(callbackSection, sp, [
+			"#access_token",
+			"scope",
+			"state",
+			"token_type",
+		]);
+		revalidationTimeout = setTimeout(revalidateAuth, 60 * 1000 * 60);
+		const nextCheck = new Date(Date.now() + 60 * 1000 * 60);
+		console.log(`Revalidation set for ${nextCheck}`);
+		return revalidationTimeout;
+	} else {
+		if (revalidationTimeout) clearTimeout(revalidationTimeout);
+		throw new Error(
+			"Failed to validate authorization on Twitch. Please reauthenticate!"
+		);
+	}
+}
+
+const viewersInput = document.getElementById("viewers");
+const chattersInput = document.getElementById("chatters");
+
+connectButton.addEventListener("click", async () => {
+	// Get username from page
+	const username = usernameInput.value;
+	// Make request to Twitch
+	const userId = await getUserId(username, headers);
+	if (!userId) {
+		throw new Error("Couldn't get userId!");
+	}
+	connectButton.innerText = `Connected to ${username} (${userId})`;
+	connectButton.disabled = true;
+	usernameInput.disabled = true;
+	try {
+		viewersInput.value = (await getViewerCount(userId, headers)) ?? "Error";
+		chattersInput.value = await getChatterCount(userId, "bostty", headers);
+	} catch (error) {
+		console.error(error);
+		reload();
+	}
+});
+
+async function getUserId(liveUserName, headers) {
+	const r = await fetch(`${HELIX}/users?login=${liveUserName}`, { headers });
+	if (!r.ok) {
+		throw new Error(
+			`Error: couldn't getUserId for ${liveUserName} (${r.status}) ${r.message}`
+		);
+	}
+	const { data } = await r.json();
+	if (!data || !data[0]) {
+		throw new Error(`Error: couldn't access data[0].id for ${liveUserName}`);
+	}
+	return data[0]["id"];
+}
+
+async function getViewerCount(liveUserId, headers) {
+	const r = await fetch(`${HELIX}/streams?user_id=${liveUserId}`, {
+		headers,
+	});
+	const { data } = await r.json();
+	console.log(data);
+	if (!data || !data[0]) {
+		throw new Error(`No viewer count data for ${liveUserId}`);
+	}
+	return data[0]["viewer_count"];
+}
+
+async function getChatterCount(liveUserId, modUserId, authHeaders) {}
+
+eventInput.addEventListener("keydown", (e) => {
+	if (e.key === "Enter") markButton.click();
+});
+
+markButton.addEventListener("click", () => {
+	const value = eventInput.value || "Event";
+	events.push({ time: Date.now(), data: value });
+	localStorage.setItem(EVENT_STORE, JSON.stringify(events));
+	eventInput.value = "";
+	eventInput.focus();
+	rerender();
+});
+
+const downloadButton = document.getElementById("downloadButton");
+downloadButton.addEventListener("click", () => {
+	let title = document.getElementById("eventsTitle").value;
+	if (!title) {
+		title = prompt("No title found, enter here or leave it empty:");
+	}
+	const d = new Date().toISOString().slice(0, 10);
+	const csvConfig = makeConfig({
+		useKeysAsHeaders: true,
+		filename: `${title}_${d}`,
+		title: `${title}_${d}`,
+		showTitle: true,
+		useBom: true,
+	});
+	if (events) {
+		const csv = generateCsv(csvConfig)(events);
+		downloadCsv(csvConfig)(csv);
+	} else {
+		console.error("No events found, didn't download");
+	}
+});
+
+/*
+ */
 
 function renderURLSearchParamsAsUL(parentElement, urlSearchParams, keyWhitelist) {
 	const ul = document.createElement("ul");
@@ -74,15 +249,12 @@ function renderURLSearchParamsAsUL(parentElement, urlSearchParams, keyWhitelist)
 }
 
 function toggleVisibility(...elements) {
+	const dur = 250;
 	elements.forEach((element) => {
-		const dur = 250;
 		const isHidden = element.hidden;
-		if (!element.style.transitionProperty)
-			element.style.transitionProperty = "opacity, transform";
-		if (!element.style.transitionDuration)
-			element.style.transitionDuration = `${dur}ms`;
-		if (!element.style.transitionTimingFunction)
-			element.style.transitionTimingFunction = "ease";
+		element.style.transitionProperty = "opacity, transform";
+		element.style.transitionDuration = `${dur}ms`;
+		element.style.transitionTimingFunction = "ease";
 		let transitionTimeout = setTimeout(() => {}, 0);
 		if (isHidden) {
 			clearTimeout(transitionTimeout);
